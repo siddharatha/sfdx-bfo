e8226f54e9eb5846aa6d50effa52b750b8bcef24
Sid <siddharatha.n@gmail.com>
2018-03-05 19:51:21 +0100

    merge files start

---
+++ commands/buildFromFiles.js
@@ -0,0 +1,62 @@
+const Promise = require("bluebird");
+const path = require("path");
+const fs = require("fs-extra");
+const xml2js = require("xml2js");
+const _ = require("lodash");
+const mergeFiles = require("../utils/mergeFiles");
+
+const promisefs = Promise.promisifyAll(fs);
+let count = 0;
+
+(function() {
+  "use strict";
+
+  module.exports = {
+    topic: "bfo",
+    command: "merge",
+    description: "merge from split files",
+    help: "bfo:merge please pass source and target folder",
+    flags: [
+      {
+        name: "srcfolder",
+        char: "s",
+        description: "source folder",
+        hasValue: true,
+        required: true
+      },
+      {
+        name: "targetfolder",
+        char: "t",
+        description: "target folder",
+        hasValue: true,
+        required: true
+      }
+    ],
+    run(context) {
+      const srcfolder = context.flags.srcfolder;
+      const targetfolder = context.flags.targetfolder;
+      const completesrcpath = path.resolve(srcfolder);
+      const completetargetpath = path.resolve(targetfolder);
+      if (fs.existsSync(completesrcpath)) {
+        fs.ensureDirSync(completetargetpath);
+        promisefs
+          .readdirAsync(completesrcpath)
+          .then(filelist => {
+            Promise.map(
+              filelist,
+              eachfilename => {
+                return readProfile(
+                  completesrcpath + "/" + eachfilename,
+                  completetargetpath
+                );
+              },
+              { concurrency: 20 }
+            );
+          })
+          .catch(ex => console.log(ex));
+      } else {
+        console.log("source or target doesn't exist");
+      }
+    }
+  };
+})();
---
+++ utils/mergeFiles.js
@@ -0,0 +1,1 @@
+function mergeFiles(srcfolder, targetfolder) {}
